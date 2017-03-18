6b5b62f09394bc10a22a1aa598099488490d78c2
Stefanie <ssmores@hotmail.com>
2017-03-18 08:08:41 +0000

    Updated food to have a break and a conditional.

--- food.rb
+++ food.rb
@@ -2,5 +2,9 @@ loop do
     puts "Input a food: "
     food = gets.strip.downcase
     break if food.empty?
-    puts "Yum, I love " + food + "!"
+    if food.downcase == 'cabbage' then
+        puts "Yuck, I hate " + food + "."
+    else
+        puts "Yum, I love " + food + "!"
+    end
 end
