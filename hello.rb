757a10e88a4b6e050e2ca698049b9d4845a74e46
Stefanie <ssmores@hotmail.com>
2017-03-18 08:42:28 +0000

    Reverse_array.rb contains a for loop. Hello.rb refactored to use the .include? <string or variable here> method.

--- hello.rb
+++ hello.rb
@@ -1,14 +1,16 @@
-# puts "What is your name?"
-# name = gets.strip.capitalize
+names = ['darth', 'voldermort', 'satan', 'lex']
+puts "What is your name?"
+name = gets.strip.capitalize
 # if name.downcase == "darth" or name.downcase == "voldermort" or name.downcase == "satan" then 
-#     puts "Go away!"
-# else
-#     puts "Hello, " + name + "!"
-# end
-
-# infinite loop! My "fav"... 
-loop do
-    puts "What is your name?"
-    name = gets.strip
+if names.include? name.downcase then
+    puts "Go away!"
+else
     puts "Hello, " + name + "!"
 end
+
+# # infinite loop! My "fav"... 
+# loop do
+#     puts "What is your name?"
+#     name = gets.strip
+#     puts "Hello, " + name + "!"
+# end
---
+++ reverse_array.rb
@@ -0,0 +1,6 @@
+"""Reverses a predetermined array and prints each item reversed."""
+
+fruits = ['apple', 'berry', 'cherry']
+fruits.reverse.each do |fruit|
+    puts fruit.reverse
+end
