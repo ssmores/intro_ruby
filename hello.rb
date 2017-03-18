06a990be9c02db1a0ece4c83c8e5260167fdcd59
Stefanie <ssmores@hotmail.com>
2017-03-18 07:19:19 +0000

    Created conditional statement in hello.rb.

--- hello.rb
+++ hello.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 name = gets.strip.capitalize
-puts "Hello, " + name + "!"
+if name.downcase == "darth" or name.downcase == "voldermort" or name.downcase == "satan" then 
+    puts "Go away!"
+else
+    puts "Hello, " + name + "!"
+end
